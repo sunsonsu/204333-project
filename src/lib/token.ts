b1608681ebba692577cb1dc455b719868fc0451c
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2025-01-15 15:33:59 +0700

    Add: Favorite function

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -1,5 +1,5 @@
 import { Payload } from "@/interface/token";
-import { SignJWT } from "jose";
+import { jwtVerify, SignJWT } from "jose";
 
 const secret = new TextEncoder().encode(process.env.SECRET_KEY);
 
@@ -7,4 +7,14 @@ export async function generateToken(payload:Payload):Promise<string> {
     const token = await new SignJWT(payload).setProtectedHeader({ alg: 'HS256' }).sign(secret);
 
     return token;
+}
+
+export async function validateToken(token:string):Promise<Payload | null> {
+    try {
+        const { payload } = await jwtVerify(token, secret);
+        const new_payload = payload as Payload
+        return new_payload;
+    } catch (error) {
+        return null;
+    }
 }
---
+++ src/model/favorite.ts
@@ -0,0 +1,26 @@
+import catchError from "@/lib/error";
+import prisma from ".";
+
+export async function favorite(uid: number, coin: string):Promise<Error | null> {
+    const upserting = prisma.favorite.upsert({
+        where: { uid_c: { uid: uid, c: coin } },
+        update: { deletedAt: null },
+        create: {
+            uid,
+            c: coin
+        }
+    });
+
+    const [_, err_update] = await catchError(upserting);
+    return err_update;
+}
+
+export async function unfavorite(uid: number, coin: string):Promise<Error | null> {
+    const update = prisma.favorite.update({
+        where: { uid_c: { uid, c: coin } },
+        data: { deletedAt: new Date() }
+    });
+
+    const [_, err_update] = await catchError(update);
+    return err_update;
+}
