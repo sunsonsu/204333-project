b6086ebe24f67c309ce5fab0223b69bde0b83e46
sunsonsu <komson281@gmail.com>
2025-02-11 23:57:00 +0700

    handling chat api & draft chat page for frontend

--- chat/src/routes/index.ts
+++ chat/src/routes/index.ts
@@ -1,5 +1,24 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
+const prisma = new PrismaClient();
+
+router.get("/api/chat/:curr", async (req, res) => {
+    const curr = req.params.curr
+    // console.log(curr)
+    try {
+        const users = await prisma.exchangeRate.findUnique({
+            where:{
+                coin: curr
+            }
+        });
+        res.json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "An error occurred while fetching users" });
+    }
+});
+
 
 export default router;
