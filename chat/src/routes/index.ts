f67a25fadf254fe6ee79fd71720605baf5218879
sunsonsu <komson281@gmail.com>
2025-02-13 00:59:45 +0700

    api for each chat (get & post) v1

--- chat/src/routes/index.ts
+++ chat/src/routes/index.ts
@@ -6,19 +6,40 @@ const prisma = new PrismaClient();
 
 //for getting the specific chat data
 router.get("/api/chat/:curr", async (req, res) => {
-    const curr = (req.params.curr).toLocaleUpperCase()
-    
-    // console.log(curr)
+
+    const curr = (req.params.curr).toLocaleUpperCase();
     try {
-        const users = await prisma.exchangeRate.findUnique({
+        //check for exchangeRate data in redis
+
+        //if redis is not work use this
+        const rate = await prisma.exchangeRate.findUnique({
             where:{
                 coin: curr
             }
         });
-        res.json(users);
+        //finding all chat of the specific coin
+        const chat = await prisma.chat.findMany({
+            where:{
+                c: curr
+            },
+            orderBy: {
+                createdAt: 'desc' //sorting the chat by latest created date
+            }
+        })
+
+        //finding username for each chat
+        const users = await Promise.all(chat.map(async (message) => {
+            const user = await prisma.user.findUnique({
+            where: {
+                id: message.uid
+            }
+            });
+            return { ...message, username: user?.name };//returning the chat data with username
+        }));
+        res.json({ rate, chat: users }); //returning the rate and chat data
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: "An error occurred while fetching users" });
+        res.status(500).json({ error: "An error occurred while fetching rate" });
     }
 });
 
@@ -26,12 +47,12 @@ router.get("/api/chat/:curr", async (req, res) => {
 router.post("/api/chat/:curr", async (req, res) => {
     const curr = req.params.curr
     // console.log(req.body)
-    const { message} = req.body;
+    const { message, uid} = req.body;
 
     try {
         const comment = await prisma.chat.create({
             data: {
-            uid: 1, //req.session.user_id as number
+            uid: uid, //change to req.session.user_id as number
             msg: message as string,
             c: curr as string
             }
@@ -39,7 +60,7 @@ router.post("/api/chat/:curr", async (req, res) => {
         res.json(comment);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: "An error occurred while fetching users" });
+        res.status(500).json({ error: "An error occurred while fetching rate" });
     }
 });
 
